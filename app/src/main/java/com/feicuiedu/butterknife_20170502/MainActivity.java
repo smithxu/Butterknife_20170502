fc57a04255445aba72c1ced633e5ee5184699b8f
gqq <gqq519@163.com>
2017-05-02 15:08:38 +0800

    旧版本的依赖方式和插件介绍

--- app/src/main/java/com/feicuiedu/butterknife_20170502/MainActivity.java
+++ app/src/main/java/com/feicuiedu/butterknife_20170502/MainActivity.java
@@ -1,7 +1,7 @@
 package com.feicuiedu.butterknife_20170502;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,17 +14,28 @@ import butterknife.ButterKnife;
 
 /**
  * 1. 添加依赖：
- *      compile 'com.jakewharton:butterknife:8.5.1'
- *      annotationProcessor 'com.jakewharton:butterknife-compiler:8.5.1'
- *
+ * compile 'com.jakewharton:butterknife:8.5.1'
+ * annotationProcessor 'com.jakewharton:butterknife-compiler:8.5.1'
+ * <p>
+ * 版本在2.2 以下
+ * 1. 项目的build.gradle里面：
+ * // 使用"android-apt"插件，用于ButterKnife
+ * classpath 'com.neenbedankt.gradle.plugins:android-apt:1.8'
+ * 2. app模块的build.gradle里面：
+ * compile 'com.jakewharton:butterknife:8.4.0'
+ * apt 'com.jakewharton:butterknife-compiler:8.4.0'
+ * 3. 顶部添加：apply plugin: 'android-apt'
+ * <p>
  * 2. 绑定控件
- *      使用Butterknife，一定要记得Butterknife.bind();
- *      @BindView(int)
- *      TextView tvShow
+ * 使用Butterknife，一定要记得Butterknife.bind();
  *
+ * @BindView(int) TextView tvShow
+ * <p>
  * 3. 绑定资源:通过调用不同的注解，完成不同的绑定
- *      @BindColor、@BindString等
- *
+ * @BindColor、@BindString等 介绍一个小工具：插件
+ * Butterknife zelezny插件
+ * 下载的位置：settings--plugins--搜索Butterknife zelezny
+ * 使用：点击到布局上，alt+insert，快速的生成控件绑定和监听
  */
 public class MainActivity extends AppCompatActivity {
 
